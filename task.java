aeb727432913c3c26d7e9a5e9b0285b4f1106edf
s1290086 <s1290086@u-aizu.ac.jp>
2022-07-12 13:07:17 +0900

    Hot: initial revision

---
+++ task.java
@@ -0,0 +1,18 @@
+public class task{
+    public static void main(String[] args){
+	int c,count1=0,count2=0;
+	System.out.println("Tossing a coin...");
+	for(int i = 1; i <= 3; i++){
+	    c = (int)Math.ceil(Math.random()*2);
+	    if(c == 1){
+		System.out.println("Round "+i+": Heads");
+		count1++;
+	    }
+	    else{
+		System.out.println("Round "+i+": Tails");
+		count2++;
+	    }
+	}
+	System.out.println("Heads: "+count1+", Tails: "+count2);
+    }
+}
