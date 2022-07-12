f27e6338390c2bf92af77eeab3ae0ca5c4449f8e
s1290086 <s1290086@u-aizu.ac.jp>
2022-07-12 13:33:30 +0900

    Victory message added

--- task.java
+++ task.java
@@ -14,5 +14,11 @@ public class task{
 	    }
 	}
 	System.out.println("Heads: "+count1+", Tails: "+count2);
+	if(count1>count2){
+	    System.out.println("You won!");
+	}
+	else{
+	    System.out.println("You lost!");
+	    }
     }
 }
